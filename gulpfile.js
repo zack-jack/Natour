4f24004bbeabc551990fc908a611a07286def6c3
Zack Jackson <z.jackson58@gmail.com>
2018-08-18 18:06:10 -0400

    Gulp browser-sync setup

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+
+gulp.task("serve", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task("bs-reload", function() {
+  browserSync.reload();
+});
+
+gulp.task("default", ["serve"], function() {
+  gulp.watch("css/*.css", function(file) {
+    if (file.type === "changed") {
+      browserSync.reload(file.path);
+    }
+  });
+  gulp.watch("*.html", ["bs-reload"]);
+});
